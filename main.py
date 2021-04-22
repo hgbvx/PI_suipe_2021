76e719734401ccd779c496922ce97048fea5bda8
hgbvx <hgbvx33@gmail.com>
2021-04-22 10:12:08 +0200

    switched logic to control package

--- main.py
+++ main.py
@@ -1,31 +1,34 @@
-from simple_pid import PID
-from scipy import signal
+import numpy
 import matplotlib.pyplot as plt
-
-pid = PID(1, 0.1, 0.05, setpoint=1)
-
+import control as co
 
 #Motor
 J = 0.0377 # moment of inertia of the rotor kg.m^2
 b = 0.006 # motor viscous friction constant N.m.s
-Ke = 0.01 # electromotive force constant V/rad/sec
-Kt = 0.01 # motor torque constant 0.01 N.m/Amp
-K = 1.847
+Ke = 1.847 # electromotive force constant V/rad/sec
+Kt = 1.847 # motor torque constant 0.01 N.m/Amp
+K = 1.847 # constants
 R = 3.08 # electric resistance
 L = 0.066 #electric inductance 0.5 H
 
 # K / JLs^2 + (Lb + RJ)s + Rb + K^2
+num = [K]
+den = [J*L,L*b+R*J,R*b+K*K]
 
-# Transfer function and setpoint
-lti = signal.lti([K], [J*L,L*b+R*J,R*b+K*K])
-t, y  = signal.step(lti)
-y = y * 1000
+# CONTROL PACKAGE
+
+G1= co.tf(num,den)
+# noinspection PyUnresolvedReferences
+t = numpy.linspace(0, 1, 1000) # time (start, stop, X) in sec
+
+t1, y1 = co.step_response(G1,t)
 
 
-plt.plot(t, y)
+plt.plot(t1,y1)
 plt.xlabel('Time [s]')
-plt.ylabel('Speed')
+plt.ylabel('I [A]')
 plt.title('Step Response')
 plt.grid()
 
 plt.show()
+
