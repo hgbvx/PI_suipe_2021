cde1b778f1c66a959c6e85885d216736c46272b1
hgbvx <hgbvx33@gmail.com>
2021-05-19 16:31:18 +0200

    Added documentation v2.0 TODO: noise

--- SUiPE.py
+++ SUiPE.py
@@ -1,31 +1,32 @@
 import control as co
 import numpy as np
 
+
 class Simulation:
 
 	@staticmethod
-	def PI(kp, ti):
+	def pi(kp, ti):
 		"""
 
 		:param kp: Gain of regulator
 		:param ti: Inertia of regulator
 		:return: Transfer function of PI
 		"""
-		tf1 = co.tf([kp],[1])
-		tf2 = co.tf([1],[ti, 0])
+		tf1 = co.tf([kp], [1])
+		tf2 = co.tf([1], [ti, 0])
 		tf3 = tf1 * tf2
 		return tf1 + tf3
 
 	@staticmethod
-	def motor_tf(parameters): # K / JLs^2 + (Lb + RJ)s + Rb + K^2
+	def motor_tf(parameters):  # K / JLs^2 + (Lb + RJ)s + Rb + K^2
 		"""
 
 		:param parameters: Dictionary with resistance and inductance of motor
 		:return: Transfer function of motor
 		"""
-		R = parameters["R"]
-		L = parameters["L"]
-		return co.tf([1],[L,R])
+		resistance = parameters["R"]
+		inductance = parameters["L"]
+		return co.tf([1], [inductance, resistance])
 
 	@staticmethod
 	def kfi(parameters):
@@ -34,8 +35,8 @@ class Simulation:
 		:param parameters: Dictionary with Kfi
 		:return: Transfer function of Kfi
 		"""
-		Kfi = parameters["K"]
-		return co.tf([Kfi],[1])
+		kfi = parameters["K"]
+		return co.tf([kfi], [1])
 
 	@staticmethod
 	def inertia_integrator(parameters):
@@ -44,12 +45,10 @@ class Simulation:
 		:param parameters: Dictionary with moment of inertia
 		:return: Transfer function of inertia integrator
 		"""
-		J = parameters["J"]
-		return co.tf([1],[J,0])
+		inertia = parameters["J"]
+		return co.tf([1], [inertia, 0])
 
 	@staticmethod
 	# TODO: transfer function
-	def noise(mean,var,samples):
-		return np.random.normal(mean,np.sqrt(var),samples)
-
-
+	def noise(mean, var, samples):
+		return np.random.normal(mean, np.sqrt(var), samples)
